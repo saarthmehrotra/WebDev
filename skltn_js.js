2143136b11a58955f0d88b29372c6fc681a6600f
saarthmehrotra <saarth.mehrotra@gmail.com>
2014-02-07 16:24:55 -0500

    jquery to about

--- skltn_js.js
+++ skltn_js.js
@@ -1,9 +1,18 @@
 $( document ).ready(function() {
 
+// Click on Nav Bar to Scroll to Projects Section on webpage
         $('#navProjects').click(function(event){
                 event.preventDefault();
                 $('html, body').animate({
                        scrollTop: $('#Projects').offset().top
+                 }, 700);
+        });
+
+// Click on Nav Bar to Scroll to About Section on webpage
+        $('#navAbout').click(function(event){
+                event.preventDefault();
+                $('html, body').animate({
+                       scrollTop: $('#About').offset().top
                  }, 700);
         });
 
